4a725d7645ad85ad97e29be010ca76aede0739b7
pedroarthursouza1012-sudo <pedroarthursouza1012@gmail.com>
2026-05-21 10:57:07 -0300

    Refactor organizar function and add sorting features

--- FIC-Aula-JS/script.js
+++ FIC-Aula-JS/script.js
@@ -1,26 +1,156 @@
+function organizar(){
 
-let pessoas = [];
+let pessoas = document.getElementById("nomes").value
+ if (!pessoas) return;
+
+let pessoa = pessoas.split(",").map(item => {
+
+const [nome, idade] = item.split("-")
+return{
+
+nome: nome.trim().toUpperCase(),
+idade: Number(idade)}})
+
+let lista = pessoa.sort((a, b) => a.idade - b.idade);
+
+const ul = document.getElementById("listaFinal")
+ul.innerHTML = ""
+
+lista.forEach(pessoa =>{
+
+const li = document.createElement("li")
+li.textContent = `${pessoa.nome} - ${pessoa.idade} anos`
+ul.appendChild(li)
+
+})}
 
 
-function enviar(){
+/*==========================================================*/
+/*==========================================================*/
+/*==========================================================*/
 
-    let nome = document.getElementById("nome").value
-    let idade = Number(document.getElementById("idade").value)
 
-    if (nome ==="" || idade <=0){
+function organizar2(){
 
-alert("Preencha corretamente")
-return
+let pessoas = document.getElementById("nomes").value
+ if (!pessoas) return;
 
+let pessoa = pessoas.split(",").map(item => {
+
+const [nome, idade] = item.split("-")
+return{
+
+nome: nome.trim().toUpperCase(),
+idade: Number(idade)}})
+
+let filtrados = pessoa.filter(pessoa => pessoa.nome.length >= 7)
+
+let lista = filtrados.sort((a, b) => a.idade - b.idade);
+
+const ul = document.getElementById("listaFinal2")
+ul.innerHTML = ""
+
+lista.forEach(pessoa =>{
+
+const li = document.createElement("li")
+li.textContent = `${pessoa.nome} - ${pessoa.idade} anos`
+ul.appendChild(li)
+
+})}
+/*==========================================================*/
+/*==========================================================*/
+/*==========================================================*/
+
+function organizarAlfa(){
+
+let pessoas = document.getElementById("nomes").value
+ if (!pessoas) return;
+
+let pessoa = pessoas.split(",").map(item => {
+
+const [nome, idade] = item.split("-")
+return{
+
+nome: nome.trim().toUpperCase(),
+idade: Number(idade)}})
+
+let nomesAlfa = pessoa.sort((a,b)=>
+
+a.nome.localeCompare(b.nome)
+);
+    
+const ul = document.getElementById("listaFinal3")
+ul.innerHTML = ""
+
+nomesAlfa.forEach( pessoa=>{
+
+const li = document.createElement("li")
+li.textContent = `${pessoa.nome} - ${pessoa.idade} anos`
+ul.appendChild(li)
+})}
+
+/*==========================================================*/
+/*==========================================================*/
+/*==========================================================*/
+
+
+function limpar(){
+
+const ul = document.getElementById("listaFinal")
+ul.innerHTML = ""
+const ul2 = document.getElementById("listaFinal2")
+ul2.innerHTML = ""
+const ul3 = document.getElementById("listaFinal3");
+    ul3.innerHTML = "";
 }
 
-pessoas.push({nome: nome, idade: idade});
+/*==========================================================*/
+/*==========================================================*/
+/*==========================================================*/
 
-let item = document.createElement("li")
-item.innerText = nome + " - " + idade + " anos";
-document.getElementById("lista").appendChild(item);
+function analise() {
 
-document.getElementById("nome").value = "";
-document.getElementById("idade").value = "";
+    let pessoas = document.getElementById("nomes").value
+ if (!pessoas) return;
 
-}
+let pessoa = pessoas.split(",").map(item => {
+
+const [nome, idade] = item.split("-")
+return{
+nome: nome.trim(),
+idade: Number(idade)
+};
+});
+
+
+let maiores = pessoa.filter(pessoa => pessoa.idade >= 18);
+    let quantidade = pessoa.length;
+    let nomes = maiores.map(pessoa => pessoa.nome);
+
+
+
+
+    let maisVelho = pessoa.reduce((maior, atual)=>{
+return atual.idade < maior.idade ? maior : atual;
+
+})
+
+    let maisNovo = pessoa.reduce((menor, atual)=>{
+
+return atual.idade < menor.idade ? atual: menor
+
+
+    })
+
+
+alert(
+        "Maiores de idade: " + nomes.join(", ") +
+        "\nQuantidade de pessoas: " + quantidade +
+        "\nMais velho: " + maisVelho.nome + " (" + maisVelho.idade + " anos)" +
+        "\nMais novo: " + maisNovo.nome + " (" + maisNovo.idade + " anos)"
+    )}
+
+/*==========================================================*/
+/*==========================================================*/
+/*==========================================================*/
+
