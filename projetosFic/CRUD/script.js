1516602f7e6f6594b980679bc6f6f0392eea838f
pedroarthursouza1012-sudo <pedroarthursouza1012@gmail.com>
2026-09-12 18:42:47 -0300

    Cenário atualizado, jogabilidade implementada e correção de bugs e responsividade adicionada e novos jogadores- Infinity run

--- projetosFic/CRUD/script.js
+++ projetosFic/CRUD/script.js
@@ -229,6 +229,19 @@ addBtn.addEventListener("click",async ()=>{
 
     if(modoEdicao){
 
+        const valor = Number(nota.value);
+
+        if (
+            nota.value.trim() === "" ||
+            isNaN(valor) ||
+            valor < 0 ||
+            valor > 10
+        ) {
+            alert("Adicione uma nota válida!");
+            nota.value = "";
+            return;
+        }
+
         pokemonEditando.nota = Number(nota.value);
         pokemonEditando.favorito = favoritoInput.checked
 
@@ -279,6 +292,19 @@ addBtn.addEventListener("click",async ()=>{
 
 
         const nome = input.value.toLowerCase().trim();
+
+        const valor = Number(nota.value);
+
+        if (
+            nota.value.trim() === "" ||
+            isNaN(valor) ||
+            valor < 0 ||
+            valor > 10
+        ) {
+            alert("Adicione uma nota válida!");
+            nota.value = "";
+            return;
+        }
 
 
         if(pokemonsAdicionados.some(pokemon => pokemon.nome === nome)){
